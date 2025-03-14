e56638537bc02ea08c78c7b8fdba45f13482fefd
Abdulrhman Kamal <abdulrhmankamal144@gmail.com>
2025-03-14 13:01:54 +0200

    Update dtoNewUser.cs

--- Models/dtoNewUser.cs
+++ Models/dtoNewUser.cs
@@ -24,7 +24,6 @@ namespace UserApi.Models
             public DateOnly BirthDate { get; set; }
             [Required(ErrorMessage = "Choose A Valid Gender")]
             public Gender SelectGender { get; set; }
-            //public List<SelectListItem> GenderList { get; set; }
 
             [Required(ErrorMessage = "Password is required.")]
             [StringLength(40, MinimumLength = 8, ErrorMessage = "The {0} must be at {2} and at max {1} characters long.")]
